3787845d0b8a81579e42d0a06d1038cf318e5afe
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-01-23 07:56:40 +0700

    Membuat Data Class Penayangan

--- app/src/main/java/com/example/finalproject/model/Cinemas.kt
+++ app/src/main/java/com/example/finalproject/model/Cinemas.kt
@@ -3,12 +3,39 @@ package com.example.finalproject.model
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+
 @Serializable
 data class Film(
-    val id_film : String,
-    val judul_film : String,
+    @SerialName("id_film")
+    val idFilm : String,
+
+    @SerialName("judul_film")
+    val judulFilm : String,
+
     val durasi : String,
     val deskripsi : String,
     val genre : String,
-    val rating_usia : String,
+
+    @SerialName("rating_usia")
+    val ratingUsia : String,
 )
+
+@Serializable
+data class Penayangan(
+    @SerialName("id_penayangan")
+    val idPenayangan : String,
+
+    @SerialName("id_film")
+    val idFilm : String,
+
+    @SerialName("id_studio")
+    val idStudio : String,
+
+    @SerialName("tanggal_penayangan")
+    val tanggalPenayangan : String,
+
+    @SerialName("harga_tiket")
+    val hargaTiket : String,
+
+)
+
