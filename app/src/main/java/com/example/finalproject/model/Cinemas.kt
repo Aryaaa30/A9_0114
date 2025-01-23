0f2895a0f8049b93b2f552ac9d77650c5b6381b5
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-01-23 08:01:56 +0700

    Membuat Data Class Tiket

--- app/src/main/java/com/example/finalproject/model/Cinemas.kt
+++ app/src/main/java/com/example/finalproject/model/Cinemas.kt
@@ -46,6 +46,24 @@ data class Studio(
     @SerialName("nama_studio")
     val namaStudio : String,
 
-    val kapasitas : String
+    val kapasitas : String,
+)
+
+@Serializable
+data class Tiket(
+    @SerialName("id_tiket")
+    val idTiket : String,
+
+    @SerialName("id_penayangan")
+    val idPenayangan : String,
+
+    @SerialName("jumlah_tiket")
+    val jumlahTiket : String,
+
+    @SerialName("total_harga")
+    val totalHarga : String,
+
+    @SerialName("status_pembayaran")
+    val statusPembayaran : String,
 )
 
