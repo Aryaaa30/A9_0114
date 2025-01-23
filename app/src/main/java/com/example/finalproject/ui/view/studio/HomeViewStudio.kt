2a59b79b759fe7d27732d5ff125268a4d9e68709
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-01-24 05:30:56 +0700

    Membuat HomeViewStudio

--- app/src/main/java/com/example/finalproject/ui/view/studio/HomeViewStudio.kt
+++ app/src/main/java/com/example/finalproject/ui/view/studio/HomeViewStudio.kt
@@ -1,20 +1,135 @@
 package com.example.finalproject.ui.view.studio
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finalproject.R
+import com.example.finalproject.model.Studio
+import com.example.finalproject.ui.PenyediaViewModel
+import com.example.finalproject.ui.costumwigdet.CostumeTopAppBar
+import com.example.finalproject.ui.navigation.DestinasiNavigasi
+import com.example.finalproject.ui.viewmodel.studio.HomeUiState
+import com.example.finalproject.ui.viewmodel.studio.HomeViewModelStudio
 
+object DestinasiHomeStudio : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Studio"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeViewStudio(
+    navigateToItemEntry: ()-> Unit,
+    modifier: Modifier=Modifier,
+    onDetailClick: (String) -> Unit={},
+    viewModel: HomeViewModelStudio = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiHomeStudio.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getStudio()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Mahasiswa")
+            }
+        }
+    ){ innerPadding ->
+        HomeStatus(
+            homeUiState = viewModel.studioUIState,
+            retryAction = { viewModel.getStudio() }, modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = {
+                viewModel.deleteStudio(it.idStudio)
+                viewModel.getStudio()
+            }
+        )
+    }
+}
+
+@Composable
+fun HomeStatus(
+    homeUiState: HomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Studio) -> Unit={},
+    onDetailClick: (String) -> Unit
+){
+    when(homeUiState) {
+        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeUiState.Success ->
+            if (homeUiState.studio.isEmpty()) {
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Studio")
+                }
+            } else {
+                StudioLayout(
+                    studio = homeUiState.studio, modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.idStudio)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+//Homescreen menampilkan loading message
 @Composable
 fun OnLoading(modifier: Modifier = Modifier){
     Image(
@@ -38,6 +153,107 @@ fun OnError(retryAction: ()->Unit, modifier: Modifier = Modifier){
         Text(text = stringResource(R.string.loading_failed),modifier = Modifier.padding(16.dp))
         Button(onClick = retryAction) {
             Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun StudioLayout(
+    studio: List<Studio>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Studio) -> Unit,
+    onDeleteClick: (Studio) -> Unit = {}
+){
+    LazyColumn (
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        items(studio){studio ->
+            StudioCard(
+                studio = studio,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(studio) },
+                onDeleteClick = {
+                    onDeleteClick(studio)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun StudioCard(
+    studio: Studio,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Studio) -> Unit = {}
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .shadow(8.dp, shape = RoundedCornerShape(12.dp)),
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFFE0F7FA)) // Warna latar belakang biru muda
+                .padding(16.dp)
+        ) {
+            // Bagian kiri: Logo atau gambar
+            Box(
+                modifier = Modifier
+                    .size(80.dp)
+                    .background(Color(0xFF0097A7), shape = RoundedCornerShape(8.dp)),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.man), // Ganti dengan logo Anda
+                    contentDescription = "Logo",
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            // Bagian kanan: Informasi mahasiswa
+            Column(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = studio.idStudio,
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.Bold
+                    ),
+                    color = Color(0xFF00796B) // Warna teks hijau tua
+                )
+                Text(
+                    text = "NIM: ${studio.namaStudio}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color(0xFF004D40)
+                )
+                Text(
+                    text = "Kelas: ${studio.kapasitas}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color(0xFF004D40)
+                )
+            }
+
+            // Tombol delete
+            IconButton(onClick = { onDeleteClick(studio) }) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete",
+                    tint = Color.Red
+                )
+            }
         }
     }
 }
