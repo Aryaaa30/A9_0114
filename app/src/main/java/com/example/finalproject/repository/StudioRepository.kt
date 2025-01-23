3eeee4213bb615a62e041349db4d7e50e0591761
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-01-23 09:00:07 +0700

    Membuat Repository dan Service dari Studio

--- app/src/main/java/com/example/finalproject/repository/StudioRepository.kt
+++ app/src/main/java/com/example/finalproject/repository/StudioRepository.kt
@@ -1,2 +1,49 @@
 package com.example.finalproject.repository
 
+import com.example.finalproject.model.Studio
+import com.example.finalproject.service.StudioService
+import java.io.IOException
+
+interface StudioRepository {
+    suspend fun getStudio(): List<Studio>
+    suspend fun insertStudio(studio: Studio)
+    suspend fun updateStudio(idStudio: String, studio: Studio)
+    suspend fun deleteStudio(idStudio: String)
+    suspend fun getStudioById(idStudio: String): Studio
+}
+
+class NetworkStudioRepository(
+    private val studioApiService: StudioService
+) : StudioRepository {
+
+
+    override suspend fun insertStudio(studio: Studio) {
+        studioApiService.insertStudio(studio)
+    }
+
+    override suspend fun updateStudio(idStudio: String, studio: Studio) {
+        studioApiService.updateStudio(idStudio, studio)
+    }
+
+    override suspend fun deleteStudio(idStudio: String) {
+        try {
+            val response = studioApiService.deleteStudio(idStudio)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete Film. HTTP Status code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getStudio(): List<Studio> =
+        studioApiService.getAllStudio()
+
+    override suspend fun getStudioById(idStudio: String): Studio {
+        return studioApiService.getStudioById(idStudio)
+    }
+}
--- app/src/main/java/com/example/finalproject/service/StudioService.kt
+++ app/src/main/java/com/example/finalproject/service/StudioService.kt
@@ -1,4 +1,33 @@
 package com.example.finalproject.service
 
+import com.example.finalproject.model.Film
+import com.example.finalproject.model.Studio
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
 interface StudioService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+    @GET("bacastudio.php")
+    suspend fun getAllStudio(): List<Studio>
+
+    @GET("baca1studio.php")
+    suspend fun getStudioById(@Query("idStudio")idStudio: String): Studio
+
+    @POST("insertstudio.php")
+    suspend fun insertStudio(@Body studio: Studio)
+
+    @PUT("editstudio.php/{idStudio}")
+    suspend fun updateStudio(@Query("idStudio")idStudio: String, @Body studio: Studio)
+
+    @DELETE("deletestudio.php/{idStudio}")
+    suspend fun deleteStudio(@Query("idStudio")idStudio: String):retrofit2.Response<Void>
 }
