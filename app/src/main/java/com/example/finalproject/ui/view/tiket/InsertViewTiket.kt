7a969694665db69dcec339d1914e39723d48a23e
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-01-24 06:15:02 +0700

    Membuat InsertViewTiket

---
+++ app/src/main/java/com/example/finalproject/ui/view/tiket/InsertViewTiket.kt
@@ -0,0 +1,163 @@
+package com.example.finalproject.ui.view.tiket
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalproject.ui.PenyediaViewModel
+import com.example.finalproject.ui.costumwigdet.CostumeTopAppBar
+import com.example.finalproject.ui.navigation.DestinasiNavigasi
+import com.example.finalproject.ui.viewmodel.tiket.InsertUiEvent
+import com.example.finalproject.ui.viewmodel.tiket.InsertUiState
+import com.example.finalproject.ui.viewmodel.tiket.InsertViewModelTiket
+import kotlinx.coroutines.launch
+
+object DestinasiInsertTiket: DestinasiNavigasi {
+    override val route ="item_entry"
+    override val titleRes = "Insert Tiket"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InsertViewTiket(
+    navigateBack: ()-> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModelTiket= viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier=modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiInsertTiket.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){ innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertTiketState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertTiket()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent)->Unit,
+    onSaveClick: ()->Unit,
+    modifier: Modifier = Modifier
+){
+    Column (
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent)->Unit={},
+    enabled: Boolean = true
+) {
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        OutlinedTextField(
+            value = insertUiEvent.idTiket,
+            onValueChange = {onValueChange(insertUiEvent.copy(idTiket = it))},
+            label = { Text("ID Tiket") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.idPenayangan,
+            onValueChange = {onValueChange(insertUiEvent.copy(idPenayangan = it))},
+            label = { Text("ID Penayangan") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.jumlahTiket,
+            onValueChange = {onValueChange(insertUiEvent.copy(jumlahTiket = it))},
+            label = { Text("Jumlah Tiket") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.totalHarga,
+            onValueChange = {onValueChange(insertUiEvent.copy(totalHarga = it))},
+            label = { Text("Total Harga") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.statusPembayaran,
+            onValueChange = {onValueChange(insertUiEvent.copy(statusPembayaran = it))},
+            label = { Text("Status Pembayaran") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled){
+            Text(
+                text = "Isi Semua Data",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
