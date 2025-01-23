61c3d766b1bd50379b55b251015e84f5be44685f
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-01-24 04:47:55 +0700

    Membuat HomeViewPenayangan

--- app/src/main/java/com/example/finalproject/ui/view/penayangan/HomeViewPenayangan.kt
+++ app/src/main/java/com/example/finalproject/ui/view/penayangan/HomeViewPenayangan.kt
@@ -1,19 +1,133 @@
 package com.example.finalproject.ui.view.penayangan
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finalproject.R
+import com.example.finalproject.model.Penayangan
+import com.example.finalproject.ui.PenyediaViewModel
+import com.example.finalproject.ui.costumwigdet.CostumeTopAppBar
+import com.example.finalproject.ui.navigation.DestinasiNavigasi
+import com.example.finalproject.ui.viewmodel.penayangan.HomeUiState
+import com.example.finalproject.ui.viewmodel.penayangan.HomeViewModelPenayangan
+
+object DestinasiHomePenayangan : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Penayangan"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeViewPenayangan(
+    navigateToItemEntry: ()-> Unit,
+    modifier: Modifier=Modifier,
+    onDetailClick: (String) -> Unit={},
+    viewModel: HomeViewModelPenayangan = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiHomePenayangan.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getPenayangan()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Penayangan")
+            }
+        }
+    ){ innerPadding ->
+        HomeStatus(
+            homeUiState = viewModel.penayanganUIState,
+            retryAction = { viewModel.getPenayangan() }, modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = {
+                viewModel.deletePenayangan(it.idPenayangan)
+                viewModel.getPenayangan()
+            }
+        )
+    }
+}
+
+@Composable
+fun HomeStatus(
+    homeUiState: HomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Penayangan) -> Unit={},
+    onDetailClick: (String) -> Unit
+){
+    when(homeUiState) {
+        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeUiState.Success ->
+            if (homeUiState.penayangan.isEmpty()) {
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Penayangan")
+                }
+            } else {
+                PenayanganLayout(
+                    penayangan = homeUiState.penayangan, modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.idPenayangan)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
 
 @Composable
 fun OnLoading(modifier: Modifier = Modifier){
@@ -38,6 +152,117 @@ fun OnError(retryAction: ()->Unit, modifier: Modifier = Modifier){
         Text(text = stringResource(R.string.loading_failed),modifier = Modifier.padding(16.dp))
         Button(onClick = retryAction) {
             Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun PenayanganLayout(
+    penayangan: List<Penayangan>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Penayangan) -> Unit,
+    onDeleteClick: (Penayangan) -> Unit = {}
+){
+    LazyColumn (
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        items(penayangan){penayangan ->
+            PenayanganCard(
+                penayangan = penayangan,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(penayangan) },
+                onDeleteClick = {
+                    onDeleteClick(penayangan)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun PenayanganCard(
+    penayangan: Penayangan,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Penayangan) -> Unit = {}
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .shadow(8.dp, shape = RoundedCornerShape(12.dp)),
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFFE0F7FA)) // Warna latar belakang biru muda
+                .padding(16.dp)
+        ) {
+            // Bagian kiri: Logo atau gambar
+            Box(
+                modifier = Modifier
+                    .size(80.dp)
+                    .background(Color(0xFF0097A7), shape = RoundedCornerShape(8.dp)),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.man), // Ganti dengan logo Anda
+                    contentDescription = "Logo",
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            // Bagian kanan: Informasi penayangan
+            Column(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = penayangan.idPenayangan,
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.Bold
+                    ),
+                    color = Color(0xFF00796B) // Warna teks hijau tua
+                )
+                Text(
+                    text = "NIM: ${penayangan.idFilm}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color(0xFF004D40)
+                )
+                Text(
+                    text = "Kelas: ${penayangan.idStudio}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color(0xFF004D40)
+                )
+                Text(
+                    text = "Alamat: ${penayangan.tanggalPenayangan}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color(0xFF004D40)
+                )
+                Text(
+                    text = "Alamat: ${penayangan.hargaTiket}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color(0xFF004D40)
+                )
+            }
+
+            // Tombol delete
+            IconButton(onClick = { onDeleteClick(penayangan) }) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete",
+                    tint = Color.Red
+                )
+            }
         }
     }
 }
