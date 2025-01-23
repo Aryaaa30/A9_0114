6a43857dcdd2f524ed7f687f5a25e0ac0584d7c7
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-01-24 05:24:30 +0700

    Membuat DetailViewStudio

---
+++ app/src/main/java/com/example/finalproject/ui/view/studio/DetailViewStudio.kt
@@ -0,0 +1,211 @@
+package com.example.finalproject.ui.view.studio
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalproject.model.Studio
+import com.example.finalproject.ui.PenyediaViewModel
+import com.example.finalproject.ui.costumwigdet.CostumeTopAppBar
+import com.example.finalproject.ui.navigation.DestinasiNavigasi
+import com.example.finalproject.ui.viewmodel.studio.DetailUiState
+import com.example.finalproject.ui.viewmodel.studio.DetailViewModelStudio
+
+object DestinasiDetailStudio: DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = "Detail Studio"
+    const val Studio = "idStudio"
+    val routesWithArg = "$route/{$Studio}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailViewStudio(
+    navigateBack: () -> Unit,
+    navigateToItemUpdate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModelStudio = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiDetailStudio.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                onRefresh = {
+                    viewModel.getStudioById()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemUpdate,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Kontak"
+                )
+            }
+        }
+    ) { innerPadding ->
+        DetailStatus(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = viewModel.studioDetailState,
+            retryAction = { viewModel.getStudioById() },
+            onDeleteClick = {
+                viewModel.deleteStudio(viewModel.studioDetailState.let { state ->
+                    if (state is DetailUiState.Success) state.studio.idStudio else ""
+                })
+                navigateBack()
+            }
+        )
+    }
+}
+
+
+@Composable
+fun DetailStatus(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState,
+    onDeleteClick: () -> Unit
+) {
+    when (detailUiState) {
+        is DetailUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is DetailUiState.Success -> {
+            if (detailUiState.studio.idStudio.isEmpty()) {
+                Box(
+                    modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center
+                ) { Text("Data tidak ditemukan") }
+            } else {
+                ItemDetailStudio(
+                    studio = detailUiState.studio,
+                    modifier = modifier.fillMaxWidth(),
+                    onDeleteClick = onDeleteClick
+                )
+            }
+        }
+        is DetailUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+
+@Composable
+fun ItemDetailStudio(
+    modifier: Modifier = Modifier,
+    studio: Studio,
+    onDeleteClick: () -> Unit
+) {
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    Card(
+        modifier = modifier.padding(16.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailStudio(judul = "ID Studio", isinya = studio.idStudio)
+            Divider(modifier = Modifier.padding(vertical = 8.dp))
+            ComponentDetailStudio(judul = "Nama Studio", isinya = studio.namaStudio)
+            Divider(modifier = Modifier.padding(vertical = 8.dp))
+            ComponentDetailStudio(judul = "Kapasitas", isinya = studio.kapasitas)
+
+            Spacer(modifier = Modifier.padding(8.dp))
+
+            Button(
+                onClick = {
+                    deleteConfirmationRequired = true
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Delete")
+            }
+
+            if (deleteConfirmationRequired) {
+                DeleteConfirmationDialog(
+                    onDeleteConfirm = {
+                        deleteConfirmationRequired = false
+                        onDeleteClick()
+                    },
+                    onDeleteCancel = { deleteConfirmationRequired = false },
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ComponentDetailStudio(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = judul,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = isinya,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+    }
+}
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = {},
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anda yakin ingin menghapus data?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        })
+}
